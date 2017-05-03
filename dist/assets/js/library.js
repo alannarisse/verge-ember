175739c2244ae1dc84814100ccc094f21c04dc16
Martin Broerse <broerse@martinic.com>
2017-05-03 04:40:14 +0200

    Add lightbox2 to bower and remove dist folder from git

--- dist/assets/js/library.js
+++
@@ -1,9 +0,0 @@
-/* Magnific popup */
-$(document).ready(function() {
-	console.log("Ready!");
-	$('.lightbox').magnificPopup({type:'image'});
-	$('.test-popup-link').magnificPopup({
-		type: 'image'
-		// other options
-	});
-});
